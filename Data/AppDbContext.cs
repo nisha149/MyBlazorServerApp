7cab4a7592809c85ae7dd71207a86c7ee3ee2a60
nisha <nisharenish14@gmail.com>
2025-09-06 12:25:09 +0530

    Initial commit - Blazor Server app with Customer management

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,26 +6,22 @@ namespace MyBlazorServerApp.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
         public virtual DbSet<Customer> Customers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Customer>()
+                .HasKey(c => c.Id); // Explicitly define primary key
+            modelBuilder.Entity<Customer>()
                 .HasQueryFilter(c => !c.IsDeleted);
-
             modelBuilder.Entity<Customer>()
                 .Property(c => c.Code).HasMaxLength(20).IsRequired();
-
             modelBuilder.Entity<Customer>()
                 .Property(c => c.Name).HasMaxLength(100).IsRequired();
-
             modelBuilder.Entity<Customer>()
                 .Property(c => c.Phone).HasMaxLength(20);
-
             modelBuilder.Entity<Customer>()
                 .Property(c => c.Gstin).HasMaxLength(20);
-
             modelBuilder.Entity<Customer>()
                 .Property(c => c.IsDeleted).HasDefaultValue(false);
         }
