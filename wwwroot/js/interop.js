26eb92f661720d43e82d13d5c6d59238aaa4c117
nisha <nisharenish14@gmail.com>
2025-10-20 10:02:46 +0530

    Initial commit for Render deployment

---
+++ wwwroot/js/interop.js
@@ -0,0 +1,13 @@
+﻿window.isMobileDevice = () => {
+    return window.innerWidth <= 768;
+};
+
+window.setupResizeListener = (dotNetObject) => {
+    const handleResize = () => {
+        const isMobile = window.innerWidth <= 768;
+        dotNetObject.invokeMethodAsync('UpdateMobileState', isMobile);
+    };
+    window.addEventListener('resize', handleResize);
+    // Initial call to set the state
+    handleResize();
+};
